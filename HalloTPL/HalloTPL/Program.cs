7b6905a4e0a0b6495fbce0401fe5cf174afaec80
André <AndreR@ppedv.de>
2023-11-14 08:58:53 +0100

    Schnelles TPL Beispiel

---
+++ HalloTPL/HalloTPL/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Parallel.For(0, 200000, x => Console.WriteLine($"{x}"));
+
+//for (int i = 0; i < 20; i++)
+//{
+//    Console.WriteLine($"{i}");
+//}
