30b041a0d617fa73dc132c0dca569b1819200faf
André <AndreR@ppedv.de>
2023-11-15 12:37:02 +0100

    GoogleBooks Client Console

---
+++ RestClient/RestClient/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RestClient;
+
+Console.WriteLine("Hello, World!");
+
+var url = "https://www.googleapis.com/books/v1/volumes?q=katze&maxResults=40&startIndex=40";
+
+var http = new HttpClient();
+var json = http.GetStringAsync(url).Result;
+
+var br = System.Text.Json.JsonSerializer.Deserialize<BooksResult>(json);
+
+Console.WriteLine($"Books: {br.totalItems}");
+
+foreach (var book in br.items.Select(x => x.volumeInfo))
+    Console.WriteLine($"{book.title}");
