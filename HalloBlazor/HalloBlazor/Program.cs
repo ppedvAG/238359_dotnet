c09a16980b5f3cd4d2e5c83a4fd0076c80e052bc
André <AndreR@ppedv.de>
2023-11-16 13:10:25 +0100

    Test with MudBlazor

--- HalloBlazor/HalloBlazor/Program.cs
+++ HalloBlazor/HalloBlazor/Program.cs
@@ -1,4 +1,5 @@
 using HalloBlazor.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddMudServices();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
