e6696b16d8b446bf5a9238407ba3164634452737
André <AndreR@ppedv.de>
2023-11-16 12:30:30 +0100

    ASP Web Api mit Pizza

---
+++ PizzaApi/PizzaApi/Controllers/PizzaController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HalloAspMVC.Data;
+using HalloAspMVC.Models;
+
+namespace PizzaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private readonly HalloAspMVCContext _context;
+
+        public PizzaController(HalloAspMVCContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pizza
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pizza>>> GetPizza()
+        {
+            return await _context.Pizza.ToListAsync();
+        }
+
+        // GET: api/Pizza/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pizza>> GetPizza(int id)
+        {
+            var pizza = await _context.Pizza.FindAsync(id);
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return pizza;
+        }
+
+        // PUT: api/Pizza/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPizza(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pizza).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PizzaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pizza
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pizza>> PostPizza(Pizza pizza)
+        {
+            _context.Pizza.Add(pizza);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPizza", new { id = pizza.Id }, pizza);
+        }
+
+        // DELETE: api/Pizza/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePizza(int id)
+        {
+            var pizza = await _context.Pizza.FindAsync(id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pizza.Remove(pizza);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PizzaExists(int id)
+        {
+            return _context.Pizza.Any(e => e.Id == id);
+        }
+    }
+}
